98180fa3bad40063d624a06f963c10e25a07b273
Re-joice <ofosuarmahrejoice@example.com>
2026-06-02 07:32:42 -0700

    Implement push and pall opcodes

--- free_stack.c
+++ free_stack.c
@@ -6,12 +6,12 @@
  */
 void free_stack(stack_t *stack)
 {
-        stack_t *tmp;
+	stack_t *tmp;
 
-        while (stack)
-        {
-                tmp = stack;
-                stack = stack->next;
-                free(tmp);
-        }
+	while (stack)
+	{
+		tmp = stack;
+		stack = stack->next;
+		free(tmp);
+	}
 }
--- main.c
+++ main.c
@@ -8,22 +8,22 @@
  */
 int is_number(char *str)
 {
-        int i = 0;
+	int i = 0;
 
-        if (str == NULL)
-                return (0);
+	if (str == NULL)
+		return (0);
 
-        if (str[0] == '-')
-                i++;
+	if (str[0] == '-')
+		i++;
 
-        while (str[i])
-        {
-                if (!isdigit(str[i]))
-                        return (0);
-                i++;
-        }
+	while (str[i])
+	{
+		if (!isdigit(str[i]))
+			return (0);
+		i++;
+	}
 
-        return (1);
+	return (1);
 }
 
 /**
@@ -35,67 +35,66 @@ int is_number(char *str)
  */
 int main(int argc, char *argv[])
 {
-        FILE *file;
-        char line[256];
-        char *opcode, *arg;
-        stack_t *stack = NULL;
-        unsigned int line_number = 0;
+	FILE *file;
+	char line[256];
+	char *opcode, *arg;
+	stack_t *stack = NULL;
+	unsigned int line_number = 0;
 
-        if (argc != 2)
-        {
-                fprintf(stderr, "USAGE: monty file\n");
-                exit(EXIT_FAILURE);
-        }
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
 
-        file = fopen(argv[1], "r");
-        if (file == NULL)
-        {
-                fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-                exit(EXIT_FAILURE);
-        }
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 
-        while (fgets(line, sizeof(line), file))
-        {
-                line_number++;
-                opcode = strtok(line, " \t\n");
+	while (fgets(line, sizeof(line), file))
+	{
+		line_number++;
+		opcode = strtok(line, " \t\n");
 
-                if (opcode == NULL)
-                        continue;
+		if (opcode == NULL)
+			continue;
 
-                if (strcmp(opcode, "push") == 0)
-                {
-                        arg = strtok(NULL, " \t\n");
+		if (strcmp(opcode, "push") == 0)
+		{
+			arg = strtok(NULL, " \t\n");
 
-                        if (!is_number(arg))
-                        {
-                                fprintf(stderr,
-                                        "L%u: usage: push integer\n",
-                                        line_number);
-                                fclose(file);
-                                free_stack(stack);
-                                exit(EXIT_FAILURE);
-                        }
+			if (!is_number(arg))
+			{
+				fprintf(stderr,
+					"L%u: usage: push integer\n",
+					line_number);
+				fclose(file);
+				free_stack(stack);
+				exit(EXIT_FAILURE);
+			}
 
-                        push(&stack, atoi(arg));
-                }
-                else if (strcmp(opcode, "pall") == 0)
-                {
-                        pall(&stack);
-                }
-                else
-                {
-                        fprintf(stderr,
-                                "L%u: unknown instruction %s\n",
-                                line_number,
-                                opcode);
-                        fclose(file);
-                        free_stack(stack);
-                        exit(EXIT_FAILURE);
-                }
-        }
+			push(&stack, atoi(arg));
+		}
+		else if (strcmp(opcode, "pall") == 0)
+		{
+			pall(&stack);
+		}
+		else
+		{
+			fprintf(stderr,
+				"L%u: unknown instruction %s\n",
+				line_number, opcode);
+			fclose(file);
+			free_stack(stack);
+			exit(EXIT_FAILURE);
+		}
+	}
 
-        fclose(file);
-        free_stack(stack);
+	fclose(file);
+	free_stack(stack);
 
-        return (0);
+	return (0);
 }
